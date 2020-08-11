400e8dfa7318301b9221ff113610583857f64efd
cx-wzl <luciferwzl@hotmail.com>
2020-08-11 17:04:19 +0800

    update

---
+++ projects/ng-cxui/schematics/ng-add/index.ts
@@ -0,0 +1,27 @@
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+} from '@angular-devkit/schematics';
+
+// Just return the tree
+export function ngAdd(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const workspaceConfigBuffer = tree.read('angular.json');
+    if (!workspaceConfigBuffer) {
+      throw new SchematicsException('Not an Angular CLI workspace');
+    }
+
+    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    const projectName = _options.project || workspaceConfig.defaultProject;
+    const project = workspaceConfig.projects[projectName];
+    project.architect.build.options.assets.push({
+      glob: '**/*',
+      input: 'node_modules/ng-cxui/assets/',
+      output: 'assets/',
+    });
+    tree.overwrite('angular.json', JSON.stringify(workspaceConfig));
+    return tree;
+  };
+}
